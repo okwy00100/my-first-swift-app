a94d3651950c4a7482d996043ba1b858bd36919a
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2021-02-14 19:28:08 +0100

    another commit

--- MySwiftApp/ViewController.swift
+++ MySwiftApp/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        func buttonAction(_ sender: UIButton) {
+        }
     }
 
 
